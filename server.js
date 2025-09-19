28d7573b2d9336a82fb59e0282d5bdee22cb57df
hej6 <hej6@southernct.edu>
2025-09-19 15:14:02 +0000

    Server now displays image5.jpg as requested

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+
+
 //call back function
-serveStatic = function (req, res) {
+serveStatic = function (req, res)
+{
 	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
+
+	fs.readFile(fileName,function (err, content)
+	{
+		if (err)
+		{
+			return err
+		}
+		else
+		{
+			res.writeHead(200, {'Content-Type': 'image/jpg'});
+			res.write(content);
+			res.end();
+		}
+		
+	});
+	
 }
  
 const myserver = http.createServer(serveStatic); //create a server object
